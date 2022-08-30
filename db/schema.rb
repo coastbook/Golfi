cdbf7e89cc18ceee4b866c6ebb90f9af7c937acc
NaoyaKishimoto <106848683+coastbook@users.noreply.github.com>
2022-08-30 14:04:21 +0000

    バグ修正

--- db/schema.rb
+++ db/schema.rb
@@ -89,9 +89,9 @@ ActiveRecord::Schema.define(version: 2022_08_22_103338) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.string "best_score"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "status", limit: 1, default: 1, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
