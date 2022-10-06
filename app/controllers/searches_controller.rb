f45117dca95469bb952e1ca2c0bc5eb8505e3b06
NaoyaKishimoto <106848683+coastbook@users.noreply.github.com>
2022-10-06 09:08:36 +0000

    readable code

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,11 +2,11 @@ class SearchesController < ApplicationController
 
   def search
     @range = params[:range]
-
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
     else
       @columns = Column.looks(params[:search], params[:word])
     end
   end
+  
 end
